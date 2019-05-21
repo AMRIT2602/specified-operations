9461eae646512553f8e239a3b441e276cd732f61
HP <HP@DESKTOP-6SUN5DR>
2019-05-22 10:47:48 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace specified_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(-1 + 4 * 6);
+            //-1 + 24 = 23
+
+            Console.WriteLine((35+5)%7) ;
+            //40 % 7 = 5 (remainder of 40/7)
+
+            Console.WriteLine(14 + -4 * 6 / 11);
+            //14 - (24/11)= 14 - 2 = 12
+
+            Console.WriteLine((2 + 15 / 6) * 1 - 7 % 2);
+            //2 + (15/6) - remainder of (7/2) = 2 + 2 - 1 = 4 - 1 = 3
+
+
+
+        }
+    }
+}
